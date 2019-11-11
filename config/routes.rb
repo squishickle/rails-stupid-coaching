2ee0b86dd39db20bd91d842ab127de55e5f84f80
Gin Atkins <vcatkins@gmail.com>
2019-11-11 12:20:03 +1100

    Without css applied yet:

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'questions#ask' # this directs it to the right page but with the shorter/more direct URL
+  get '/answer', to: 'questions#answer' #these are all controllers that it is looking up
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
